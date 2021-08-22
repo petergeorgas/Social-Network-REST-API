71f3bf17713dfb02630418bd35789eb7294428d4
Peter Georgas <peter_georgas@yahoo.com>
2021-08-22 17:08:43 -0400

    Re-organized file structure

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express'); // Add express
-const jwt = require('jsonwebtoken');
-const User = require('./models/user');
-const bcrypt = require('bcrypt'); // Import bcrypt for hashing the password 
 const mongoose = require('mongoose'); // Import mongoose for model stuff
 const middleware = require('./middleware');
-require('dotenv').config();
+const authRoute = require('./routes/auth');
+
+
 
 const CONNECTION_URL = process.env.CONNECTION_STRING
-const token_secret = process.env.TOKEN_SECRET
 const DB_NAME = process.env.DB_NAME;
 const PORT = process.env.PORT || 3000;
 
@@ -20,72 +18,10 @@ app.use(middleware.enableCrossOrigin);
 
 
 app.use(express.json()); // Apply json middleware
+app.use('/api/auth', authRoute);
 
-const port = 3000; // We will use port 8080 for this API 
 
-
-const userRouter = express.Router();
-// Registering (creating an account) 
-userRouter.route('/register')
-    .post((request, response) => {
-        const {firstName, lastName, email, pass} = request.body; // Get the body 
-        
-        // Hash the password 
-        bcrypt.hash(pass, 10, (err, hash) => {
-            if(err) {
-                response.status(500).json(error); 
-            } else {
-                // Create a new user matching our mongodb schema
-                const newUser = User({
-                    firstName: firstName, 
-                    lastName: lastName, 
-                    email: email,
-                    pass: hash
-                }); 
-                newUser.save()
-                    .then(user => {
-                        response.status(200).json({token: generateToken(user)});
-                    })
-                    .catch(error => {
-                        response.status(500).json(error); 
-                    });
-            }
-        })
-    });
-
-// Logging in 
-userRouter.get('/login', (request, response) => {
-    User.findOne({email: request.body.email})
-    .then( user => {
-        if(!user) {
-            response.status(404).json({error: "No user found"});
-        } else {
-            bcrypt.compare(request.body.pass, user.pass, (err, match) => {
-                if(err) {
-                    response.status(500).json(err); 
-                } else if (match) {
-                    response.status(200).json({token: generateToken(user)});
-                } else {
-                    response.status(403).json({error: "Passwords do not match"});
-                }
-            });
-        }
-    })
-    .catch(error => {
-        response.status(500).json(error);
-    })
-});
-
-app.use(userRouter);
-
-const journalRouter = express.Router();
-journalRouter.route('/entries')
-    .get((request, response) => {
-        return response.status(200);
-    });
-app.use(journalRouter);
-
-app.listen(port, () => {
+app.listen(PORT, () => {
     mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}, (error, client) => {
         if(error) {
             throw error;
@@ -100,7 +36,3 @@ app.listen(port, () => {
     });
 });
 
-// Generate a token 
-function generateToken(user) {
-    return jwt.sign({data: user}, token_secret, {expiresIn: '24h'}); // Sign a certificate using our secret token that expires in 1 day... 
-}
---
+++ routes/auth.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt'); 
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const token_secret = process.env.TOKEN_SECRET;
+
+
+// Logging in 
+router.get('/login', (request, response) => {
+    User.findOne({email: request.body.email})
+    .then( user => {
+        if(!user) {
+            response.status(404).json({error: "No user found"});
+        } else {
+            bcrypt.compare(request.body.pass, user.pass, (err, match) => {
+                if(err) {
+                    response.status(500).json(err); 
+                } else if (match) {
+                    response.status(200).json({token: generateToken(user)});
+                } else {
+                    response.status(403).json({error: "Passwords do not match"});
+                }
+            });
+        }
+    })
+    .catch(error => {
+        response.status(500).json(error);
+    })
+});
+
+router.post('/register', (request, response) => {
+    const {firstName, lastName, email, pass} = request.body; // Get the body 
+    
+    // Hash the password 
+    bcrypt.hash(pass, 10, (err, hash) => {
+        if(err) {
+            response.status(500).json(error); 
+        } else {
+            // Create a new user matching our mongodb schema
+            const newUser = User({
+                firstName: firstName, 
+                lastName: lastName, 
+                email: email,
+                pass: hash
+            }); 
+            newUser.save()
+                .then(user => {
+                    response.status(200).json({token: generateToken(user)});
+                })
+                .catch(error => {
+                    response.status(500).json(error); 
+                });
+        }
+    })
+});
+
+// Generate a token 
+function generateToken(user) {
+    return jwt.sign({data: user}, token_secret, {expiresIn: '24h'}); // Sign a certificate using our secret token that expires in 1 day... 
+}
+
+module.exports = router;
